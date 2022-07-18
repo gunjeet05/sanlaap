3de2ace5f8abda34c4f62595fdb909cc2416c0c0
GUNJEET KUMAR SINGH <gunjeet.singh9971@gmail.com>
2022-07-18 21:13:39 +0530

    Added modal to chat room top

---
+++ src/Components/chatWindow/ChatTop/RoominfoButtonModal.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button, Modal } from 'rsuite'
+import { useCurrentRoom } from '../../../Context/Currentroom.context'
+import { useModalState } from '../../../misc/CustomHook'
+
+const RoominfoButtonModal = () => {
+
+    const name = useCurrentRoom(v => v.name)
+    const description = useCurrentRoom(v => v.description)
+    const { isOpen, close, open } = useModalState();
+
+    return (
+        <div>
+            <Button appearance='link' className='px-0' onClick={open} >
+                Room Information
+            </Button>
+
+
+            <Modal show={isOpen} onHide={close}>
+
+                <Modal.Header>
+                    <Modal.Title>
+                        {name}
+                    </Modal.Title>
+
+                </Modal.Header>
+
+                <Modal.Body>
+                    <h6 className='mb-1'>
+                        Description of the room
+
+                    </h6>
+                    <p>
+                        {description}
+                    </p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button block onClick={close}>Close Modal</Button>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+    )
+}
+
+export default RoominfoButtonModal
--- src/Components/chatWindow/ChatTop/index.js
+++ src/Components/chatWindow/ChatTop/index.js
@@ -1,15 +1,38 @@
 import React, { memo } from 'react'
+import { Link } from 'react-router-dom'
+import { Icon , ButtonToolbar} from 'rsuite'
 import { useCurrentRoom } from '../../../Context/Currentroom.context'
+import { useMediaQuery } from '../../../misc/CustomHook'
+import RoominfoButtonModal from './RoominfoButtonModal'
 
 const Chatttop = () => {
 const name=useCurrentRoom( v=>v.name )
 console.log("name in chat top", name)
 
+const isMobile=useMediaQuery('(max-width:992px)')
+
   
   return (
-    <div>
-      { name }
+   <div>
+    <div className='d-flex justify-content-between align-items-center'>
+      <h4>
+        < Icon componentClass={Link}  to='/' 
+        icon='arrow-circle-left' size='2x' className={isMobile?'d-inline-block p-0 mr-2 text-blue link-unstyled':'d-none'} />
+      
+      <span className='text-disappear'>{name}</span>
+      </h4>
+      <ButtonToolbar className="whits-nowrap">Todo </ButtonToolbar>
+
     </div>
+
+
+
+    <div className='d-flex justify-content-between align-items-center'>
+
+      <span>Todo</span>
+      <RoominfoButtonModal />
+    </div>
+   </div>
   )
 }
 
