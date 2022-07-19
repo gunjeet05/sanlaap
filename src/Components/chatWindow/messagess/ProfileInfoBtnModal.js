231f5eccf3abb618757c46aa68c3c140dea7b670
GUNJEET KUMAR SINGH <gunjeet.singh9971@gmail.com>
2022-07-19 20:37:48 +0530

    fixed errors displaying user profile

--- src/Components/chatWindow/messagess/ProfileInfoBtnModal.js
+++ src/Components/chatWindow/messagess/ProfileInfoBtnModal.js
@@ -3,11 +3,11 @@ import { Button, Modal } from 'rsuite';
 import { useModalState } from '../../../misc/CustomHook';
 import ProfileAvatar from '../../ProfileAvatar';
 
-const ProfileInfoBtnModal = ({author, ...btnprops}) => {
-    const {name, avatar, createdAt}=author;
+const ProfileInfoBtnModal = ({profile, ...btnprops}) => {
+    const {name, avatar, createdAt}=profile;
     const {isOpen, close, open }=useModalState()
     const shortName=name.split(" ")[0];
-    console.log("I am running ", shortName);
+    console.log("I am running s", shortName);
     const menberSince=new Date(createdAt).toLocaleDateString();
   return (
     <>
@@ -15,11 +15,11 @@ const ProfileInfoBtnModal = ({author, ...btnprops}) => {
     <Modal show={isOpen} onHide={close}>
         <Modal.Header>
             <Modal.Title>
-                {shortName}
+               About  {shortName}
             </Modal.Title>
         </Modal.Header>
         <Modal.Body className='text-center'>
-            <ProfileAvatar src={avatar} name={name} />
+            <ProfileAvatar src={avatar} name={name} className="width-200 height-200 img-fullsize font-huge" />
 
             <h4 className='mt-2'>{name}</h4>
             <p>member since{menberSince}</p>
