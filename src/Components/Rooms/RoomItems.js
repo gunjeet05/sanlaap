8c0f71b29bf4125b5d55946c85890a7aba67d8ef
GUNJEET KUMAR SINGH <gunjeet.singh9971@gmail.com>
2022-07-19 11:55:13 +0530

    displaying last message user avatar and last message time in room list

--- src/Components/Rooms/RoomItems.js
+++ src/Components/Rooms/RoomItems.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import TimeAgo from 'react-timeago'
+import ProfileAvatar from '../ProfileAvatar';
 
 const RoomItems = ({room}) => {
 
-  const {createdAt, name}=room;
+  const {createdAt, name, lastmessage}=room;
+  console.log("last message format", lastmessage);
+
 
   return (
     <div>
   <div className='d-flex justify-content-between align-items-center'>
     <h3 className='text-disappear'>{name}</h3>
-    <TimeAgo date={new Date(createdAt)}  className='font-normal text-black-45'/>
+    <TimeAgo 
+    date={lastmessage ? new Date(lastmessage.createdAt) : new Date(createdAt)}  className='font-normal text-black-45'/>
   </div>
 
   <div className='d-flex align-items-center text-black-70'>
 
-    <span >No Messages Yet</span>
+  {lastmessage?
+  <>
+<div className='d-flex align-items-center'>
+  <ProfileAvatar 
+  src={lastmessage.author.avatar}
+  name={lastmessage.author.name}
+  size="sm"
+  />
+</div> 
+
+<div className='text-disappear ml-2' >
+  <div className=' italic'>{lastmessage.author.name}</div>
+  <span>{lastmessage.text}</span>
+</div>
+  </>:
+  
+  <span >No Messages Yet</span>
+  
+
+}
   </div>
     </div>
   )
